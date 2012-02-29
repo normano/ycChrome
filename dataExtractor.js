13f4692aa2580b4b95c7ea2f8b4ff79d89ab53ca
Norman Ovenseri <novenseri@gmail.com>
2012-02-29 11:37:27 -0600

    Updating to v0.3. Changing css styles for options and coding style.

--- dataExtractor.js
+++ dataExtractor.js
@@ -1,55 +1,53 @@
-function removePunc(str){
-	return str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
-}
-
-function normalizeString(str)
-{
-	return removePunc(str).toLowerCase();
-}
-
 // Send request to extension to see if its online
-chrome.extension.sendRequest({request: "available"}, function(response){
-	extOnline = response.status;
+var request = {request: "available"};
+chrome.extension.sendRequest(request, initializeRequest);
+
+// Start up the data extraction
+function initializeRequest(response)
+{
+	extOnline = response.status; // Do I?
 
 	// Check if extension is online
 	if(extOnline == "yes")
 	{
-		// Data Extraction
-		
+		///// Data Extraction
 		// Extract titles from page, except the more link
 		var titlesEle = $('.title a').slice(0, -1);
-		
 		var articleData = []; // Array of objects is better
+		
+		// Extract titles
 		$(titlesEle).each(function(){
 			var titleElement = $(this);
 			articleData.push({title: normalizeString(titleElement.text()), link: titleElement.attr('href')});
 		})
 		
-		// create hash from title and link
-		// When we go to the page, tell extension toadd our articles
-		$(document).ready(function(event){
+		// Setting up listeners
+		$(document).ready(dataReady); // We are ready
+		$(window).delegate(".title a", 'click', titleClick);// Add a listener to listen for clicks on the elements
+		$(window).bind('beforeunload', pageUnload); // On unload, tell extension to remove our articles
+		
+		function dataReady(event)
+		{
 			// Store articles from current tab
 			chrome.extension.sendRequest({request: "addTabData", data: articleData});
-		});
+			// Send the titles to the classifier
+			chrome.extension.sendRequest({request: "classify", data: articleData}, classify);
+		}
 		
-		// This may be redundant..., look towards pushing this functionality to the onload event
-		// Send these titles to the classifier
-		chrome.extension.sendRequest({request: "classify", data: articleData}, 
-			function(response){
-				var articleElements = titlesEle; // Not sure why I named this titlesEle
-				// Response will consist of title indices that have been labeled interesting
-				var titleIndices = response.titleIndices;
-				
-				$.each(titleIndices, function(index){
-					// Change color
-					$(articleElements[titleIndices[index]]).css({backgroundColor: "#C03010", color: "#FFF"});
-				});
-			}
-		);
+		function classify(response)
+		{
+			var articleElements = titlesEle; // Not sure why I named this titlesEle
+			// Response will consist of title indices that have been labeled interesting
+			var titleIndices = response.titleIndices;
+			
+			$.each(titleIndices, function(index){
+				// Change color
+				$(articleElements[titleIndices[index]]).css({backgroundColor: "#C03010", color: "#FFF"});
+			});
+		}
 		
-		// Add a listener to listen for clicks on the elements
-		$(window).delegate(".title a", 'click', function(event){
-		
+		function titleClick(event)
+		{
 			var element = $(this);
 			// If we clicked the more button to process dataset
 			if( element.text() === "More" )
@@ -64,12 +62,21 @@ chrome.extension.sendRequest({request: "available"}, function(response){
 			chrome.extension.sendRequest({request: "label", dataPoint: {title: textNormalized, link:link, label: 1}});
 			
 			//event.preventDefault(); // Debugging purposes			
-		});
+		}
 		
-		// On unload, tell extension to remove our articles
-		$(window).bind('beforeunload', function(event){
+		function pageUnload(event)
+		{
 			// Send title elements for training
 			chrome.extension.sendRequest({request: "removeTabData"});
-		});
+		}
 	}
-});
+}
+
+function removePunc(str){
+	return str.replace(/[^\w\s]|_/g, "").replace(/\s+/g, " ");
+}
+
+function normalizeString(str)
+{
+	return removePunc(str).toLowerCase();
+}
