f39f3ea16150eb30e955e7d03f7cd085498302ca
Norman Ovenseri <novenseri@gmail.com>
2012-07-22 09:05:37 -0700

    Added new files for better/advanced text processing.

--- dataExtractor.js
+++ dataExtractor.js
@@ -6,28 +6,29 @@ chrome.extension.sendRequest(request, initializeRequest);
 function initializeRequest(response)
 {
 	extOnline = response.status; // Do I?
+	var titlesEle = {};
 
 	// Check if extension is online
 	if(extOnline == "yes")
 	{
-		///// Data Extraction
-		// Extract titles from page, except the more link
-		var titlesEle = $('.title a').slice(0, -1);
-		var articleData = []; // Array of objects is better
-		
-		// Extract titles
-		$(titlesEle).each(function(){
-			var titleElement = $(this);
-			articleData.push({title: normalizeString(titleElement.text()), link: titleElement.attr('href')});
-		})
-		
 		// Setting up listeners
-		$(document).ready(dataReady); // We are ready
+		$(document).ready(pageReady); // We are ready
 		$(window).delegate(".title a", 'click', titleClick);// Add a listener to listen for clicks on the elements
 		$(window).bind('beforeunload', pageUnload); // On unload, tell extension to remove our articles
 		
-		function dataReady(event)
+		function pageReady(event)
 		{
+			///// Data Extraction
+			// Extract titles from page, except the more link
+			titlesEle = $('.title a').slice(0, -1);
+			var articleData = []; // Array of objects is better
+			
+			// Extract titles
+			$(titlesEle).each(function(){
+				var titleElement = $(this);
+				articleData.push({title: normalizeString(titleElement.text()), link: titleElement.attr('href')});
+			});
+			
 			// Store articles from current tab
 			chrome.extension.sendRequest({request: "addTabData", data: articleData});
 			// Send the titles to the classifier
@@ -36,7 +37,7 @@ function initializeRequest(response)
 		
 		function classify(response)
 		{
-			var articleElements = titlesEle; // Not sure why I named this titlesEle
+			var articleElements = titlesEle;
 			// Response will consist of title indices that have been labeled interesting
 			var titleIndices = response.titleIndices;
 			var recTextColors = response.textColors;
@@ -63,7 +64,8 @@ function initializeRequest(response)
 			var link = element.attr('href');
 			chrome.extension.sendRequest({request: "label", dataPoint: {title: textNormalized, link:link, label: 1}});
 			
-			//event.preventDefault(); // Debugging purposes			
+			event.preventDefault(); // Yeah....
+			window.open(link, '_newtab');
 		}
 		
 		function pageUnload(event)
---
+++ tests/specs/NaiveBayesSpec.js
@@ -0,0 +1,5 @@
+describe('Naive Bayes',function(){
+	it('Classify', function(){
+		expect();// Something
+	});
+});
